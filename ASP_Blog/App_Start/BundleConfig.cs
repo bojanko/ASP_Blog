1012c7be2af2cbf3a65cf06d05a4fb75071fb301
bojanko <31866364+bojanko@users.noreply.github.com>
2018-03-27 04:56:44 +0200

    Inicijalna struktura
    Struktura projekta i templejt

---
+++ ASP_Blog/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ASP_Blog
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/jquery-3.3.1.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/bootstrap.bundle.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/Bootstrap/bootstrap-grid.min.css",
+                "~/Content/Bootstrap/bootstrap-reboot.min.css",
+                "~/Content/Bootstrap/bootstrap.min.css"));
+        }
+    }
+}
---
+++ ASP_Blog/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASP_Blog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /*PAGES ROUTE*/
+            routes.MapRoute(
+                name: "Pages",
+                url: "{action}",
+                defaults: new { controller = "Page", action = "Home" }
+            );
+        }
+    }
+}
